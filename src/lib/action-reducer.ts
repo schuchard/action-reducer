411e8dd7c6ab343a9675af85ac74d78518a50a81
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-12-19 22:37:48 -0700

    add mismatch class name, type guard for dev

--- src/lib/action-reducer.ts
+++ src/lib/action-reducer.ts
@@ -8,9 +8,22 @@ export interface ActionReducer extends Action {
 export function actionReducer(actionModule: object, initialState: any) {
   return (state = initialState, action: Action) => {
     for (const [actionType, actionClass] of Object.entries(actionModule)) {
+      checkNameMisMatch(action, actionClass);
+
       if (action.type === actionType) return new actionClass().reduce(state);
     }
 
     return state;
   };
+}
+
+function checkNameMisMatch(actionType: object, actionClass: any) {
+  if (environment.production) return;
+  const storeAction = Object.getPrototypeOf(actionType).constructor;
+  // check if class name differs from `type` field in class;
+  if (storeAction.name !== 'Object' && storeAction.name !== new actionClass().constructor.name) {
+    console.warn(
+      `name mismatch, expected ${storeAction.name}, got ${new actionClass().constructor.name}`
+    );
+  }
 }
