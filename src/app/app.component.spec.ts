69bec47dbfeb4f0b356067f9d44048b17ab7bc46
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-12-19 23:28:40 -0700

    rename to action reducer

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wrap-reducer'`, () => {
+  it(`should have as title 'action-reducer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('wrap-reducer');
+    expect(app.title).toEqual('action-reducer');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to wrap-reducer!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to action-reducer!');
   });
 });
