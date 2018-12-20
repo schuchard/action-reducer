95a9a0d573d92087ba6291e3072a882969b4e582
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-12-19 22:46:13 -0700

    add store devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 import { counterReducer } from './store/counter1.reducer';
 import * as counter2Actions from './store/counter2.actions';
 import { actionReducer } from 'src/lib/action-reducer';
@@ -17,6 +19,10 @@ import { actionReducer } from 'src/lib/action-reducer';
     StoreModule.forRoot({
       count1: counterReducer,
       count2: actionReducer(counter2Actions, 0),
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
     }),
   ],
   providers: [],
